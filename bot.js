b69726bd879882821329c835ec67610b0c3938a6
Volzyfp <52263392+Volzyfp@users.noreply.github.com>
2019-06-28 11:32:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,3 +2,5 @@ client.on('guildMemberAdd', member => {
     const logChannel = member.guild.channels.get("593906554160414730);
     logChannel.send(Hello its **depht**);
 });
+
+client.login(process.env.TOKEN);
